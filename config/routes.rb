bcec64f5680306c65897ed01d76a5fc2e93f5a8a
Bojan Batalo <bbatalo@hotmail.com>
2018-03-03 20:41:15 +0100

    Fix up home page, routing and styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'help', to: 'static_pages#help'
 
-  get 'users/new'
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
