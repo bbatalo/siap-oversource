d7e5ae7d205b5a0ca532c234c29ee65e8401bb7c
Bojan Batalo <bbatalo@hotmail.com>
2018-03-03 23:42:24 +0100

    Add admin indicator to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303173751) do
+ActiveRecord::Schema.define(version: 20180303223435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180303173751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
