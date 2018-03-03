72601850004feb3eb2c26846200a5fc71f09c928
Bojan Batalo <bbatalo@hotmail.com>
2018-03-03 20:16:39 +0100

    Add home and help pages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :password, presence: true
+  has_secure_password
 end
