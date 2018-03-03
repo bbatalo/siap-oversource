974b209ac948836cf1f0ce2acf364309a9abb3b5
Bojan Batalo <bbatalo@hotmail.com>
2018-03-03 22:48:01 +0100

    Minor refactor.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome to Oversource! \nWORK FOR US. NOW.'
+      flash[:success] = 'Welcome to Oversource! WORK FOR US. NOW.'
       redirect_to @user
     else
       render 'new'
